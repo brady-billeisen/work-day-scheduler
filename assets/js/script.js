2f10156d24ea740c2a58ca1d2945f5f75fd77831
brady <bradybill36@gmail.com>
2023-05-17 21:58:40 -0500

    starter code links were messed up so i fixed those, now im gonna keep working on the script

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,21 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  var hour = dayjs().hour() - 9;
+  console.log(hour)
+  $('.time-block').each(function() {
+    var id = $(this).attr('id').split('-')[1];
+    var rowHour = parseInt(id);
+    console.log(rowHour);
+
+    if (hour < rowHour) {
+      $(this).addClass('future');
+    } else if (hour > rowHour) {
+      $(this).addClass('past');
+    } else {
+      $(this).addClass('present');
+    }
+  });
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
